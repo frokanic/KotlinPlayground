7d5baed799cbd4b67fc557326626a9454330b94f
frokanic <84749437+frokanic@users.noreply.github.com>
2025-03-15 13:00:57 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.frokanic
+
+fun main() {
+    println("Hello World!")
+}
---
+++ src/main/kotlin/functions/ScopeFunctions.kt
@@ -0,0 +1,34 @@
+package org.frokanic.functions
+
+/**
+ *  Sources:
+ *      - https://kotlinlang.org/docs/scope-functions.html
+ *      - https://www.youtube.com/watch?v=Vy-dS2SVoHk
+ */
+fun main() {
+
+}
+
+/**
+ *  Return Value: The lambda result.
+ *  Context Object: it
+ */
+fun let() {
+
+}
+
+fun run() {
+
+}
+
+fun with() {
+
+}
+
+fun apply() {
+
+}
+
+fun also() {
+
+}
