af3ba5955661315d0566bb239334efb2d2a5ded6
frokanic <84749437+frokanic@users.noreply.github.com>
2025-03-15 16:03:36 +0200

    Add code for the let scope function.

--- src/main/kotlin/functions/ScopeFunctions.kt
+++ src/main/kotlin/functions/ScopeFunctions.kt
@@ -6,15 +6,31 @@ package org.frokanic.functions
  *      - https://www.youtube.com/watch?v=Vy-dS2SVoHk
  */
 fun main() {
-
+    println("Starting let...")
+    let()
+    println("Let finished...")
 }
 
 /**
  *  Return Value: The lambda result.
  *  Context Object: it
+ *
+ *
  */
-fun let() {
+private var number: Int? = null
+private var number2: Int? = 1
 
+fun let() {
+    number?.let { currentNum ->
+        val number3 = currentNum + 1
+        println("number2 = $number3")
+    }
+
+    val number4 = number2?.let { currentNum ->
+        currentNum + 1
+    }
+
+    println("number3 = $number4")
 }
 
 fun run() {
