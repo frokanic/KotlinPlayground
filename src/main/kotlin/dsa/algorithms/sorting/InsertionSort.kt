4789f0321298fe79e4fa73204bf1f85836bf90b1
frokanic <84749437+frokanic@users.noreply.github.com>
2025-03-23 02:07:29 +0200

    Add Insertion Sort Code.

--- src/main/kotlin/dsa/algorithms/sorting/InsertionSort.kt
+++ src/main/kotlin/dsa/algorithms/sorting/InsertionSort.kt
@@ -1,9 +1,30 @@
 package org.frokanic.dsa.algorithms.sorting
 
 /**
- *
+ *  Time Complexity: O(n^2)
  *
  */
 fun main() {
+    insertionSortExamples(1, arrayOf(8, 7, 9, 2, 15, 4, 6))
+    println()
+    insertionSortExamples(2, arrayOf(1, 2, 4, 9, 15, 23))
+    println()
+    insertionSortExamples(3, arrayOf(90, 80, 7, 6, 4))
+    println()
+    insertionSortExamples(4, arrayOf(2, 7, 3, 1, 5, 3))
+}
 
+fun Array<Int>.insertionSort() {
+
+
+}
+
+private fun insertionSortExamples(
+    noOfExample: Int,
+    array: Array<Int>
+) {
+    println("Example $noOfExample")
+    println("Initial array          ---> ${ array.contentToString() }")
+    array.insertionSort()
+    println("Insertion sorted array ---> ${ array.contentToString() }")
 }
