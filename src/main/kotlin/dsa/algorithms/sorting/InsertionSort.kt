40680ce38cf7f8cd3afbb4f2c3ae8273e3c26504
frokanic <84749437+frokanic@users.noreply.github.com>
2025-03-25 22:25:09 +0200

    Create the implementation of insertion sort.

--- src/main/kotlin/dsa/algorithms/sorting/InsertionSort.kt
+++ src/main/kotlin/dsa/algorithms/sorting/InsertionSort.kt
@@ -3,6 +3,12 @@ package org.frokanic.dsa.algorithms.sorting
 /**
  *  Time Complexity: O(n^2)
  *
+ *  Insertion sort iterates through all of the elements, one by one, starting from the
+ *  second one, all the way to the end. It compares them with all the previous ones,
+ *  again, one by one. If the current element is smaller than the previous one, they
+ *  switch place (assuming we are ordering the array in ascending order). If the element
+ *  is not lesser that its previous one, we move one to comparing the next element to
+ *  its previous ones.
  */
 fun main() {
     insertionSortExamples(1, arrayOf(8, 7, 9, 2, 15, 4, 6))
@@ -16,7 +22,15 @@ fun main() {
 
 fun Array<Int>.insertionSort() {
 
+    for (i in 2..<this.size) {
+        for (j in i - 1 downTo 1) {
+            if (this[i] >= this[j]) break
 
+            val temp = this[i]
+            this[i] = this[j]
+            this[j] = temp
+        }
+    }
 }
 
 private fun insertionSortExamples(
