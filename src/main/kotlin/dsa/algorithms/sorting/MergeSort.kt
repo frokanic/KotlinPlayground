f0391df0d8b88aca3449f24eb6ac0891bf193342
frokanic <84749437+frokanic@users.noreply.github.com>
2025-04-05 14:55:59 +0300

    Create files for merge and radix sort.

--- src/main/kotlin/dsa/algorithms/sorting/MergeSort.kt
+++ src/main/kotlin/dsa/algorithms/sorting/MergeSort.kt
@@ -1,9 +1,33 @@
 package org.frokanic.dsa.algorithms.sorting
 
 /**
- *
+ *  Time Complexity: O(n logn)
  *
  */
 fun main() {
+
+}
+
+fun Array<Int>.mergeSortSortedArrays(
+    array2: Array<Int>
+) {
+
+
+
+}
+
+fun Array<Int>.mergeSort(
+    array2: Array<Int>
+) {
+
+
+
+}
+
+private fun mergeSortExamples(
+    noOfExample: Int,
+    array1: Array<Int>,
+    array2: Array<Int>
+) {
 
 }
---
+++ src/main/kotlin/dsa/algorithms/sorting/RadixSort.kt
@@ -0,0 +1,2 @@
+package org.frokanic.dsa.algorithms.sorting
+
